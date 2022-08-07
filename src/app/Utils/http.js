7caa031fa8796152b1ed58a12fda92d6210508c2
Ariel <epofmip@gmail.com>
2022-08-07 02:48:21 -0300

    Created rtk abstraction layer (struct)

--- src/app/Utils/http.js
+++ src/app/Utils/http.js
@@ -1,31 +1,5 @@
-import appInfo from "../../settings.json";
+const defautlHeaders = { 'Content-type': 'application/json' };
 
-export const get = (endpoint) =>
-  fetch(appInfo.BASE_URL + endpoint).then((res) => res.json());
-
-export const post = async (endpoint, body, accessToken) => {
-  const headers = accessToken
-    ? {
-        Authorization: `Bearer ${accessToken}`,
-        "Content-type": "application/json",
-      }
-    : { "Content-type": "application/json" };
-
-  const res = await fetch(appInfo.BASE_URL + endpoint, {
-    method: "POST",
-    body: JSON.stringify(body),
-    headers,
-  });
-
-  return await res.json();
-};
-
-export const patch = (endpoint, body, headers) =>
-  fetch(appInfo.BASE_URL + endpoint, {
-    method: "PATCH",
-    body: JSON.stringify(body),
-    headers: {
-      ...headers,
-      "Content-type": "application/json",
-    },
-  }).then((res) => res.json());
+// Variable name structure: [httpMethod][related][type]
+export const postWithoutTokenMutation = (url, body) => ({ url, method: "POST", body, headers: defautlHeaders }); // It is only used in login and registration requests.
+export const postMutation = (url, body, accessToken) => ({ url, method: "POST", body, headers: { ...defautlHeaders, Authorization: `Bearer ${accessToken}` } }); // This method is the default for http post.
