ef97baabe284654fe61817d3889d257580badf7c
Ariel <epofmip@gmail.com>
2022-08-07 02:54:01 -0300

    Home basic struct created

---
+++ src/app/Home/home.slice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { sessionApi } from "../Session/session.api";
+
+const initialState = {
+    user: {
+        avatarId: null,
+        name: null,
+        surname: null,
+
+    },
+};
+
+export const homeSlice = createSlice({
+    name: "homeSlice",
+    initialState,
+    reducers: {},
+    extraReducers: builder => {
+        builder.addMatcher(sessionApi.endpoints.login.matchFulfilled, (state, { payload }) => {
+            const { avatarId, name, surname } = payload;
+
+            state.user = { avatarId, name, surname };
+        }); // Match with the login endpoint
+    }
+});
+
+// export const { } = homeSlice.actions;
+export default homeSlice.reducer;
