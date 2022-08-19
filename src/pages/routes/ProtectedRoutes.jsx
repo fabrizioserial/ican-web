a3442ea81967586a537ab2877e76eca705a97bee
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2022-08-19 11:51:26 -0300

    fix:merged branch with dev feat: created protected routes

---
+++ src/pages/routes/ProtectedRoutes.jsx
@@ -0,0 +1,11 @@
+import { Navigate } from "react-router-dom";
+import React from "react";
+import { Outlet } from "react-router";
+
+export const ProtectedRoute = ({ accessToken, children }) => {
+    if (!accessToken) {
+        return <Navigate to="/login" replace />;
+    }
+
+    return children ? children : <Outlet />;
+};
