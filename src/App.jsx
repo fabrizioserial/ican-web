d5b1cf99f26e2b1c735fa247b118d2b79743b03b
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2022-08-19 15:50:26 -0300

    fix:prtected routes works

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import {Route, Routes} from "react-router";
-import {ProtectedRoute} from "./pages/routes/ProtectedRoutes";
+import React, {useEffect} from "react";
+import {Route, Routes, useNavigate} from "react-router";
+import {ProtectedRoute} from "./pages/routes/ProtectedRoute";
 import HomeScreen from "./pages/Home/HomeScreen";
 import LoginScreen from "./pages/Login/LoginScreen";
 import {useSelector} from "react-redux";
-import authState from "./redux/slices/authSlice";
 import {BrowserRouter} from "react-router-dom";
 
 
 const App = () => {
     const accessToken = useSelector((state) => state.authState.accessToken);
-    console.log(accessToken)
+
+
     return (
         <BrowserRouter>
           <Routes>
                 <Route element={<ProtectedRoute accessToken={accessToken} />}>
-                    <Route path="/" element={<HomeScreen />} />
+                    <Route path="/home" element={<HomeScreen />} />
                 </Route>
                 <Route path="/login" element={<LoginScreen />} />
                 <Route path="*" element={<div/>} />
