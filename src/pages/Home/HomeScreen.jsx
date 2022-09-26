1767cfe0103137f3919e0a2aa5e628a09f918f6c
fabrizioserial <fabrizio.serial@ing.austral.edu.ar>
2022-09-26 10:36:15 -0300

    update: package.json

--- src/pages/Home/HomeScreen.jsx
+++ src/pages/Home/HomeScreen.jsx
@@ -1,39 +1,40 @@
-import React from 'react'
-import {StyledBox} from "../../common/styledCommonComponents";
-import WidgetHome from "../../componenets/WidgetHome";
-
+import React from 'react';
+import { StyledBox } from '../../common/styledCommonComponents';
+import WidgetHome from '../../componenets/WidgetHome';
 
 function HomeScreen() {
-
-    return (
-        <StyledBox css={{
-            display: "flex",
-            width: "100vw",
-            minHeight: "100vh"
-        }}>
-            <StyledBox css={{
-                display: "flex",
-                flexDirection:"row",
-                margin:"30px 60px",
-                height:"inherit",
-                width:"inherit",
-             }}>
-                <StyledBox css={{
-                    display: "flex",
-                    flexDirection:"column",
-                    flex:0.6,
-                    maxWidth:"760px",
-                }}>
-                    <WidgetHome/>
-                </StyledBox>
-                <StyledBox css={{display:"flex", flex: 0.4}}>
-
-                </StyledBox>
-
-            </StyledBox>
-
-        </StyledBox>
-    )
+	//to remove this comment
+	return (
+		<StyledBox
+			css={{
+				display: 'flex',
+				width: '100vw',
+				minHeight: '100vh',
+			}}
+		>
+			<StyledBox
+				css={{
+					display: 'flex',
+					flexDirection: 'row',
+					margin: '30px 60px',
+					height: 'inherit',
+					width: 'inherit',
+				}}
+			>
+				<StyledBox
+					css={{
+						display: 'flex',
+						flexDirection: 'column',
+						flex: 0.6,
+						maxWidth: '760px',
+					}}
+				>
+					<WidgetHome />
+				</StyledBox>
+				<StyledBox css={{ display: 'flex', flex: 0.4 }}></StyledBox>
+			</StyledBox>
+		</StyledBox>
+	);
 }
 
 export default HomeScreen;
