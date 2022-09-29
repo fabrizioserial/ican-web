f95440f2439e7c99bb64f149442902a59238ac8f
RevJuanma <juanmarevinski1@gmail.com>
2022-09-29 18:28:19 -0300

    patients list components

---
+++ src/componenets/PatientsList/PatientContainer/styles.js
@@ -0,0 +1,31 @@
+import { StyledBox, StyledButton } from "../../../common/styledCommonComponents";
+import styled from 'styled-components';
+
+export const StyledPatientContainer = styled(StyledBox)`
+    justify-content: flex-start;
+    padding: 12px;
+    display: flex;
+    flex-direction: row;
+    border-radius: 20px;
+    box-shadow: 0 2px 24px rgba(214, 203, 252, 0.3);
+    cursor: pointer;
+    &:hover{
+        box-shadow: 0 2px 24px rgba(214, 203, 252, 0.6);
+    }
+`;
+
+export const StyledButtonMore = styled(StyledButton)`
+    border: 1px solid;
+    border-color: ${(props) => props.theme.OncoLightPurple};
+    border-radius: 20px;
+    width: 100%;
+    background-color: transparent;
+    padding: 6px;
+    color: ${(props) => props.theme.OncoPurple};
+    cursor: pointer;
+    transition: all 0.2s ease-out;
+    &:hover{
+        background-color: ${(props) => props.theme.OncoLightPurple};
+        color: ${(props) => props.theme.white};
+    }
+`;
---
+++ src/componenets/PatientsList/index.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import { useTheme } from 'styled-components';
+import { StyledBox, StyledCardHome, StyledP } from '../../common/styledCommonComponents';
+import PatientContainer from './PatientContainer';
+import { StyledButtonMore } from './PatientContainer/styles';
+import patients from './patients';
+
+const PatientsList = () => {
+    const theme = useTheme();
+    return (
+        <StyledCardHome
+            css={{
+                minHeight: "100%",
+                maxHeight: "100%",
+                width: "380px",
+                color: theme.oncoBlack,
+            }}
+        >
+            <StyledBox
+                css={{
+                    height: '24px',
+                    marginBottom: '16px',
+                }}
+            >
+                <StyledP css={{ color: theme.OncoBlack, fontWeight: 500 }}>
+                    Mis pacientes
+                </StyledP>
+            </StyledBox>
+            {/* <StyledBox css={{
+                height: "95%",
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+                justfyContent: "space-between",
+                rowGap: "10px"
+            }}>
+                {patients?.map((patient, index) => (
+                    <PatientContainer key={index} name={patient.name} surename={patient.surname} cancerType={patient.cancerType} />
+                ))}
+                <StyledButtonMore
+                    onClick={() => console.log("asd")}
+                >
+                    Ver más
+                </StyledButtonMore>
+            </StyledBox> */}
+            <StyledBox css={{
+                height: "95%",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+            }}>
+                <StyledBox css={{
+                    display: "flex",
+                    flexDirection: "column",
+                    rowGap: "10px"
+                }}>
+                    {
+                        patients?.length < 10 ?
+                            patients?.slice(0, 9).map((patient, index) => (
+                                <PatientContainer key={index} name={patient.name} surename={patient.surname} cancerType={patient.cancerType} />
+                            ))
+                            :
+                            patients?.slice(0, 8).map((patient, index) => (
+                                <PatientContainer key={index} name={patient.name} surename={patient.surname} cancerType={patient.cancerType} />
+                            ))
+                    }
+                </StyledBox>
+                {patients?.length > 9 &&
+                    (
+                        <StyledButtonMore
+                            onClick={() => console.log("asd")}
+                        >
+                            Ver más
+                        </StyledButtonMore>
+                    )}
+            </StyledBox>
+        </StyledCardHome >
+    )
+}
+
+export default PatientsList
