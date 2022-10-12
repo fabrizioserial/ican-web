aba267e2f48776109ff2fbf76076d0c1f271238c
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2022-10-12 10:55:19 -0300

    feat: styling card that will contain patient details

--- src/pages/PatientProfile/index.js
+++ src/pages/PatientProfile/index.js
@@ -1,11 +1,22 @@
 import React from "react";
-import {StyledBox} from "../../common/styledCommonComponents";
+import {StyledBox, StyledCardHome} from "../../common/styledCommonComponents";
+import {useTheme} from "styled-components";
 
 
 const PatientProfile = () => {
+    const theme = useTheme();
     return (
-        <StyledBox>
+        <StyledBox css>
         Patient Profile
+            <StyledCardHome css={{
+                position: 'absolute',
+                width: '1002 px',
+                height: '193 px',
+                left: '108 px',
+                top: '42 px',
+                background: theme.white,
+                borderRadius: '20px',
+            }}/>
         </StyledBox>
     );
 };
