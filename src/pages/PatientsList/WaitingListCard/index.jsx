3f6e63c794ab304f8bf99df022c9d40167963934
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2022-10-19 11:35:17 -0300

    feat:Added patients in waiting list

--- src/pages/PatientsList/WaitingListCard/index.jsx
+++ src/pages/PatientsList/WaitingListCard/index.jsx
@@ -1,16 +1,26 @@
 import {useTheme} from "styled-components";
-import {StyledBox, StyledP, StyledPatientsListCard} from "../../../common/styledCommonComponents";
-import React from "react";
+import {StyledBox, StyledImg, StyledP, StyledPatientsListCard} from "../../../common/styledCommonComponents";
+import React, {useMemo} from "react";
+import {patientsReport} from "../../../redux/api/homeApi";
 
 function WaitingListHeaderCard() {
     const theme = useTheme();
+    const users = useMemo(
+        () => ({
+            waitingUsers: {
+                amount: patientsReport.pending,
+                users: ['', '', '', ''], // TODO AVATAR
+            },
+        }),
+        [patientsReport],
+    );
 
     return (
         <StyledPatientsListCard css={{paddingLeft:"24px",
-            paddingRight:"29px",
+            paddingRight:"19px",
             paddingTop:"22px",
             paddingBottom:"22px"}}>
-                <StyledBox css={{display:"flex",flexDirection:"row",rowGap:"17px",flexWrap: "wrap",columnGap:"100px"}}>
+                <StyledBox css={{display:"flex",flexDirection:"row",rowGap:"17px",flexWrap: "wrap",columnGap:"70px"}}>
                     <StyledBox css={{display:"flex",flexDirection:"column",columnGap:"17px",rowGap: "14px",flexWrap: "wrap"}}>
                         <StyledP css={{
                             width: "107px",
@@ -34,7 +44,7 @@ function WaitingListHeaderCard() {
                             letterSpacing: "0.05em",
                             color: "#5F5F5F",}}>10</StyledP>
                     </StyledBox>
-                    <StyledBox css={{alignItems:"flex-end"}}>
+                    <StyledBox css={{alignItems:"flex-end",display:"flex",flexDirection:"column",justifyContent: "flex-end"}}>
                     <StyledP css={{
                         width: "43px",
                         height: "13px",
@@ -43,8 +53,36 @@ function WaitingListHeaderCard() {
                         fontSize: "11px",
                         lineHeight: "13px",
                         display: "flex",
-                        alignSelf: "flex-end",
-                        color: theme.OncoPurple,flexDirection:"row", whiteSpace: "nowrap"}}> Ver más</StyledP>
+                        alignSelf: "top",
+                        color: theme.OncoPurple,
+                        flexDirection:"row",
+                        whiteSpace: "nowrap"}}> Ver más</StyledP>
+
+                        <StyledBox
+                            css={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                marginTop: '10px',
+                            }}
+                        >
+                            {users?.waitingUsers?.users?.map((value, index) => (
+                                <StyledImg
+                                    key={index}
+                                    css={{
+                                        marginLeft: '-5px',
+                                        border: '1px solid white',
+                                        width: 22,
+                                        height: 22,
+                                        borderRadius: '50px',
+                                        imageRendering: '',
+                                        boxShadow: "0px 4px 24px rgba(214, 203, 252, 0.3)",
+                                    }}
+                                    src={
+                                        'https://media.discordapp.net/attachments/411201278031560708/1023937441264054302/default_user.png'
+                                    }
+                                />
+                            ))}
+                        </StyledBox>
                     </StyledBox>
                 </StyledBox>
         </StyledPatientsListCard>
