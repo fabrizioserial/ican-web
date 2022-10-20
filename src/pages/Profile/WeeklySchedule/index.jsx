6acdb98ab6becb9cfd087a60ea03b35da68c4716
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2022-10-20 11:04:07 -0300

    feat:created card

--- src/pages/Profile/WeeklySchedule/index.jsx
+++ src/pages/Profile/WeeklySchedule/index.jsx
@@ -6,7 +6,14 @@ import {
 
 const WeeklySchedule = () => {
     return (
-      <StyledBox> this is my weekly schedule</StyledBox>
+      <StyledBox> this is my weekly schedule
+      <StyledBox css={{
+          width: "305px",
+          height: "200px",
+          background: "#FFFFFF",
+          boxShadow: "0px 4px 24px rgba(214, 203, 252, 0.15)",
+          borderRadius: "15px"}}></StyledBox>
+      </StyledBox>
     );
 };
 
