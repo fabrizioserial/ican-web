c8f30006ae918d831af3ee31505bccf5fa8dbd8a
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2022-10-20 12:40:36 -0300

    feat:added back icon and calendar icon

--- src/pages/Profile/WeeklySchedule/index.jsx
+++ src/pages/Profile/WeeklySchedule/index.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import {
-    StyledBox,
+    StyledBox, StyledP,
 } from '../../../common/styledCommonComponents';
+import CalendarIcon from "../../../assets/CalendarIcon";
 
 
 const WeeklySchedule = () => {
     return (
-      <StyledBox> this is my weekly schedule
+      <StyledBox>
       <StyledBox css={{
           width: "305px",
           height: "200px",
           background: "#FFFFFF",
           boxShadow: "0px 4px 24px rgba(214, 203, 252, 0.15)",
-          borderRadius: "15px"}}></StyledBox>
+          borderRadius: "15px"}}>
+          <CalendarIcon/>
+          <StyledP css={{
+              width: "167px",
+              height: "16px",
+              fontStyle: "normal",
+              fontWeight: 500,
+              fontSize: "14px",
+              lineHeight: "17px",
+              display: "flex",
+              alignItems: "center",
+              letterSpacing: "0.05em",
+              textTransform: "capitalize2",
+              color: "#AF7EFF",}}> Agenda Semanal</StyledP>
+
+      </StyledBox>
       </StyledBox>
     );
 };
