b8c1725aabc287621c116ae8127e460e7565c7b8
fabrizioserial <fabrizio.serial@ing.austral.edu.ar>
2022-10-26 10:16:59 -0300

    feat: add improvement

--- src/components/DailyColumnChart/index.jsx
+++ src/components/DailyColumnChart/index.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import React, { useMemo } from 'react';
 import Chart from 'react-apexcharts';
 import ExclamamtionIcon from '../../assets/ExclamamtionIcon';
 import { StyledBox, StyledH3 } from '../../common/styledCommonComponents';
@@ -6,76 +6,86 @@ import { DailyColumnChartConfig } from '../../utils/chartsConfigs';
 import Card from '../Card';
 
 const DailyColumnChart = ({ data }) => {
-    const options = useMemo(() => DailyColumnChartConfig(data.data), [data.data]);
-    const height = "90%"
+	const options = useMemo(
+		() => DailyColumnChartConfig(data.data),
+		[data.data],
+	);
+	const height = '90%';
 
-    return (
-        <Card
-            title={'Tasa de respuesta en la Encuesta diaria'}
-            icon={<ExclamamtionIcon />}
-            width="100%"
-            height="268px"
-        >
-            <StyledBox
-                css={{ display: "flex", flexDirection: "row", width: "100%", height: height }}
-            >
-                <StyledBox
-                    css={{
-                        flex: 0.1,
-                        padding: 15,
-                        display: "flex",
-                        justifyContent: "space-around",
-                    }}
-                >
-                    <StyledBox css={{
-                        height: '100%',
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "space-between",
-                        paddingRight: 7,
-                    }} >
-                        <StyledH3
-                            css={{
-                                margin: '0 0 0 10px',
-                                textAlign: 'left',
-                                fontSize: '10px',
-                                fontWeight: 'normal',
-                                // width: '100%'
-                            }}
-                        >
-                            Pacientes
-                        </StyledH3>
-                        <StyledH3
-                            css={{
-                                // width: '100%',
-                                margin: '0 0 0 10px',
-                                textAlign: 'left',
-                                fontSize: '10px',
-                                fontWeight: 'normal',
-                            }}
-                        >
-                            {data.mounth}
-                        </StyledH3>
-                    </StyledBox>
-                    <StyledBox css={{
-                        height: 'inhert',
-                        width: 1,
-                        backgroundColor: '#E1D1FC',
-                    }} />
-                </StyledBox>
-                <StyledBox
-                    css={{ flex: 0.9,  paddingRight: 15, paddingBottom: 15 }}
-                >
-                    <Chart
-                        options={options.options}
-                        series={options.series}
-                        type="bar"
-                        height={'100%'}
-                        width='100%'
-                    />
-                </StyledBox>
-            </StyledBox>
-            {/* <StyledBox
+	return (
+		<Card
+			title={'Tasa de respuesta en la Encuesta diaria'}
+			icon={<ExclamamtionIcon />}
+			width="100%"
+			height="268px"
+		>
+			<StyledBox
+				css={{
+					display: 'flex',
+					flexDirection: 'row',
+					width: '100%',
+					height: height,
+				}}
+			>
+				<StyledBox
+					css={{
+						flex: 0.1,
+						padding: 15,
+						display: 'flex',
+						justifyContent: 'space-around',
+					}}
+				>
+					<StyledBox
+						css={{
+							height: '100%',
+							display: 'flex',
+							flexDirection: 'column',
+							justifyContent: 'space-between',
+							paddingRight: 7,
+						}}
+					>
+						<StyledH3
+							css={{
+								margin: '0 0 0 10px',
+								textAlign: 'left',
+								fontSize: '10px',
+								fontWeight: 'normal',
+								// width: '100%'
+							}}
+						>
+							Pacientes
+						</StyledH3>
+						<StyledH3
+							css={{
+								// width: '100%',
+								margin: '0 0 0 10px',
+								textAlign: 'left',
+								fontSize: '10px',
+								fontWeight: 'normal',
+							}}
+						>
+							{data.mounth}
+						</StyledH3>
+					</StyledBox>
+					<StyledBox
+						css={{
+							height: '80%',
+							width: 1,
+							backgroundColor: '#949494',
+						}}
+					/>
+				</StyledBox>
+				<StyledBox css={{ flex: 0.9, paddingRight: 15, paddingBottom: 15 }}>
+					<Chart
+						options={options.options}
+						series={options.series}
+						type="bar"
+						height={'100%'}
+						width="100%"
+					/>
+				</StyledBox>
+			</StyledBox>
+			{/* <StyledBox
                 css={{
                     display: "flex",
                     flexDirection: "row",
@@ -131,8 +141,8 @@ const DailyColumnChart = ({ data }) => {
                     />
                 </StyledBox>
             </StyledBox> */}
-        </Card>
-    );
-}
+		</Card>
+	);
+};
 
-export default DailyColumnChart
+export default DailyColumnChart;
