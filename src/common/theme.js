9d71df2a04a298e962d30a78e33d46e17fffd6f1
fabrizioserial <fabrizio.serial@ing.austral.edu.ar>
2022-10-26 09:34:24 -0300

    feat: add waiting component

--- src/common/theme.js
+++ src/common/theme.js
@@ -38,7 +38,7 @@ export const theme = {
 	oncoOrange: '#ECA485',
 	oncoOrangeTitle: '#7E4228',
 	oncoOrangeSubtitle: 'rgba(126, 66, 40, 0.4)',
-	oncoLightOrange3: '#EA8053',
+	oncoLightOrange3: 'rgba(234,128,83,0.3)',
 
 	oncoDarkGrey: '#555555',
 	oncoNavbarSelection: 'rgba(175, 126, 255, 0.08)',
