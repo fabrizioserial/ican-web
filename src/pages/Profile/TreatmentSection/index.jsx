c2cbdc75cc2db29d248748a344ca704078432b3e
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2022-10-26 22:01:13 -0300

    feat: added new treatment button

--- src/pages/Profile/TreatmentSection/index.jsx
+++ src/pages/Profile/TreatmentSection/index.jsx
@@ -5,6 +5,7 @@ import {useTheme} from "styled-components";
 import Card from "../../../components/Card";
 import CalendarIcon from "../../../assets/CalendarIcon";
 import PillIcon from "../../../assets/PillIcon";
+import PlusCircleIcon from "../../../assets/PlusCircleIcon";
 
 const TreatmentSection = () => {
     const theme = useTheme();
@@ -17,7 +18,9 @@ const TreatmentSection = () => {
            align={"top"}
         >
             <>
-                <StyledBox>
+                <StyledBox css={ {display:"flex",
+                    flexDirection:"column", flexWrap: "wrap",
+                    rowGap: "497px"}}>
                  <StyledBox css={{display:"flex",
                      flexDirection:"column",
                      justifyContent:"flex-start",
@@ -73,10 +76,43 @@ const TreatmentSection = () => {
                              textTransform: "capitalize",
                              color: "#AF7EFF",
                              whiteSpace: 'nowrap'}}>Comienzo 09/11/2021</StyledP>
-                     </StyledBox>
+                     </StyledBox >
 
-                 </StyledBox>
+                 </StyledBox >
+                    <StyledBox >
+                    <StyledBox css={{boxSizing: "border-box",
+                        width: "255px",
+                        height: "31px",
+                        background: "#FFFFFF",
+                        border: "1px solid #E1D1FC",
+                        boxShadow: "0px 4px 24px rgba(214, 203, 252, 0.15)",
+                        borderRadius: "20px",
+                        padding:"7px 62px",
+                        display:"flex",
+                        flexDirection:"row",
+                        columnGap:"5px",
+                        alignItems:"center"}}>
 
+                        <PlusCircleIcon/>
+                        <StyledP css={{
+                            width: "108px",
+                            height: "13px",
+                            fontStyle: "normal",
+                            fontWeight: 400,
+                            fontSize: "11px",
+                            lineHeight: "13px",
+                            display: "flex",
+                            alignItems: "center",
+                            letterSpacing: "0.05em",
+                            textTransform: "capitalize",
+                            color: "#AF7EFF",
+                            whiteSpace: 'nowrap'}}>
+                            Nuevo Tratamiento
+                        </StyledP>
+                    </StyledBox>
+
+
+                </StyledBox>
                 </StyledBox>
 
             </>
